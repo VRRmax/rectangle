b7f50d86b3377390243caac2e5f1c34d70a4ad13
Maximilien <maximilien.pressense@protonmail.com>
2020-06-25 16:03:25 +0200

    error handling when wrong arguments size given

--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -2,8 +2,35 @@ require 'spec_helper'
 load 'rectangle'
 
 describe 'rectangle' do
+  context 'with errors' do
+    it do
+      output = %x[./rectangle 1]
+      expected =<<~OUTPUT
+      Error: Wrong arguments
+      Usage: ./rectangle ${width} ${length}
+      OUTPUT
+      expect(output).to eq expected
+    end
+
+    it do
+      output = %x[./rectangle 1 2 3]
+      expected =<<~OUTPUT
+      Error: Wrong arguments
+      Usage: ./rectangle ${width} ${length}
+      OUTPUT
+      expect(output).to eq expected
+    end
+  end
+
   it do
-    output = %x[./rectangle]
+    output = %x[./rectangle 1 2]
+    expected =<<~OUTPUT
+    lol
+    OUTPUT
+    expect(output).to eq expected
+  end
+  xit do
+    output = %x[./rectangle 4 4]
     expected =<<~OUTPUT
     ABBC
     B  B
