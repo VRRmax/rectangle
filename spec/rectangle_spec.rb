4df7de4b4a014a11d6028224b365714dd1c1a191
Maximilien <maximilien.pressense@protonmail.com>
2020-06-25 15:51:14 +0200

    Bootstrapping test framework for TDD

--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 load 'rectangle'
 
-describe 'lol' do
-  it 'display lol' do
-    expect(main).to eq 'lol'
+describe 'rectangle' do
+  it do
+    output = %x[./rectangle]
+    expected =<<~OUTPUT
+    ABBC
+    B  B
+    DBBE
+    OUTPUT
+    expect(output).to eq expected
   end
 end
